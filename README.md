6fba2c373952b9771554973571933d3c175efa80
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2023-01-18 11:12:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# JournalObservatory-Web
+
+How to run:
+- Install dependencies with `npm install`
+- Run the local server with `npm start`
+- Open http://localhost:3000
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default App;
