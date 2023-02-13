35669e8b8f7c7cd8f62dd57163d0fa628c85da45
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2023-02-13 13:13:02 +0100

    Add content container

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import "./styles.css";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Buffer } from "buffer";
-import DetailsComponent from "./pages/details";
-import SearchComponent from "./pages/search";
-import DetailsJsonComponent from "./components/pad_json";
-import { Provider } from "react-redux";
-import store from "./store";
+import { Typography } from "@mui/material";
+
+import AppRouter from "./AppRouter";
+import "./styles.css";
 
 window.Buffer = Buffer;
 
 function App() {
     return (
-        <Provider store={store}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<SearchComponent />} />
-                    <Route path="/pad/:id" element={<DetailsComponent />} />
-                    <Route path="/raw/:id" element={<DetailsJsonComponent />} />
-                </Routes>
-            </BrowserRouter>
-        </Provider>
+        <div id="content-container">
+            <Typography variant="h4">Scholarly Communication Platform Browser</Typography>
+            <AppRouter />
+        </div>
     );
 }
 
