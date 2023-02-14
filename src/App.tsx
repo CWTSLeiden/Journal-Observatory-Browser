f1b66565421c121ea3267d4528287af67d32cedb
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2023-02-14 09:42:12 +0100

    Replace div with MUI Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Buffer } from "buffer";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 import AppRouter from "./AppRouter";
 import "./styles.css";
@@ -9,10 +9,10 @@ window.Buffer = Buffer;
 
 function App() {
     return (
-        <div id="content-container">
+        <Container id="content-container">
             <Typography variant="h4">Scholarly Communication Platform Browser</Typography>
             <AppRouter />
-        </div>
+        </Container>
     );
 }
 
