4b0cb10c2d4186109257d723ac09133efa79aabd
Bram van den Boomen <bram.boomen@gmail.com>
2023-02-14 12:47:52 +0100

    Add app header
    - add header component
    - move header out of content container
    - add MUI appbar & toolbar
    - some styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Buffer } from "buffer";
-import { Container, Typography } from "@mui/material";
-
-import AppRouter from "./AppRouter";
+import { Container, Stack } from "@mui/material";
 import "./styles.css";
+import AppHeader from "./components/header";
+import AppRouter from "./AppRouter";
 
 window.Buffer = Buffer;
 
 function App() {
     return (
-        <Container id="content-container">
-            <Typography variant="h4">Scholarly Communication Platform Browser</Typography>
-            <AppRouter />
-        </Container>
+        <Stack spacing={2}>
+            <AppHeader />
+            <Container id="content-container">
+                <AppRouter />
+            </Container>
+        </Stack>
     );
 }
 
