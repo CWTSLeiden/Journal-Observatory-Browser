05fe9fec40a96dc50ed95d2e2818a92d7ae4e8b6
Bram van den Boomen <bram.boomen@gmail.com>
2023-02-15 18:05:15 +0100

    limit depth of recursion in fold functions

--- src/query/fold.tsx
+++ src/query/fold.tsx
@@ -1,12 +1,14 @@
-function fold_graph(graph: Array<object>): Array<object> {
+function fold_graph(graph: Array<object>, depth=10): Array<object> {
     return graph.map((g) => {
-        return fold_object(g, graph);
+        const topgraph = structuredClone(graph)
+        return fold_object(g, topgraph, depth);
     });
 }
 
-function fold_object(obj: object, topgraph: Array<object>) {
+function fold_object(obj: object, topgraph: Array<object>, depth: number) {
+    if (depth < 1) { return undefined }
     for (const [k, v] of Object.entries(obj)) {
-        const new_value = fold_value(v, topgraph);
+        const new_value = fold_value(v, topgraph, depth-1);
         if (new_value && new_value != v) {
             obj[k] = new_value;
         }
@@ -14,20 +16,19 @@ function fold_object(obj: object, topgraph: Array<object>) {
     return obj;
 }
 
-function fold_value(val: Array<object>, topgraph: Array<object>): Array<object>;
-function fold_value(val: string, topgraph: Array<object>): string;
-function fold_value(val: object, topgraph: Array<object>): object;
-function fold_value(val: unknown, topgraph: Array<object>): unknown {
+function fold_value(val: Array<object>, topgraph: Array<object>, depth: number): Array<object>;
+function fold_value(val: string, topgraph: Array<object>, depth: number): string;
+function fold_value(val: object, topgraph: Array<object>, depth: number): object;
+function fold_value(val: unknown, topgraph: Array<object>, depth: number): unknown {
+    if (depth < 1) { return undefined }
     if (Array.isArray(val) && val.length > 0) {
         return val.map((v: object) => {
-            return fold_value(v, topgraph);
+            return fold_value(v, topgraph, depth) || v;
         });
     }
-    const valmatches = topgraph.filter((g) => {
-        return g["@id"] == val["@id"];
-    });
-    if (valmatches.length > 0) {
-        return fold_object(valmatches[0], topgraph);
+    const match = topgraph.filter((g) => g["@id"] == val["@id"]).find(Boolean)
+    if (match) {
+        return fold_object(match, topgraph, depth-1) || match;
     }
     return val;
 }
