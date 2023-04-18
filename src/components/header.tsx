3ed17b438c95d49f5a961a10c2edec4a5670bf6e
Nees Jan van Eck <ecknjpvan@cwts.leidenuniv.nl>
2023-04-18 16:25:40 +0200

    Add logo in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,12 @@
-import { AppBar, Container, Toolbar, Typography } from "@mui/material";
 import React from "react";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import { LibraryBooks } from "@mui/icons-material";
 
 const AppHeader = () => (
     <AppBar position="static" elevation={0}>
         <Container id="header-container">
             <Toolbar>
+                <LibraryBooks sx={{paddingRight: '10px'}}/>
                 <Typography component="h1" variant="h6">
                     Scholarly Communication Platform Browser
                 </Typography>
